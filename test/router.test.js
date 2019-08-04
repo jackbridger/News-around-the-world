b6ba437e9ae8a43264aeb3b749d963025f7eabf5
Jack Bridger <jacksbridger@gmail.com>
2019-08-04 13:51:26 +0100

    uncomment old tests that should be used

--- test/router.test.js
+++ test/router.test.js
@@ -19,27 +19,27 @@ tape("Home route returns a status code of 200", t => {
     });
 });
 
-// tape("Search returns a status code of 200 and a JSON object", t => {
-//   supertest(router)
-//     .get("/search?fr")
-//     .expect(200)
-//     .expect("Content-Type", "application/json")
-//     .end((err, res) => {
-//       t.error(err);
-//       t.end();
-//     });
-// });
+tape("Search returns a status code of 200 and a JSON object", t => {
+  supertest(router)
+    .get("/search?fr")
+    .expect(200)
+    .expect("Content-Type", "application/json")
+    .end((err, res) => {
+      t.error(err);
+      t.end();
+    });
+});
 
-// tape("Search returns a status code of 200 and any file in public folder", t => {
-//   supertest(router)
-//     .get("/public.css")
-//     .expect(200)
-//     .expect("Content-type", /css/)
-//     .end((err, res) => {
-//       t.error(err);
-//       t.end();
-//     });
-// });
+tape("Search returns a status code of 200 and any file in public folder", t => {
+  supertest(router)
+    .get("/search?us")
+    .expect(200)
+    .expect("Content-type", "application/json")
+    .end((err, res) => {
+      t.error(err);
+      t.end();
+    });
+});
 
 tape("Return an error if the route is wrong", t => {
   supertest(router)
