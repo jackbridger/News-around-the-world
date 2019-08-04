945bb02015d07e3bf5d5ec088222fed9d52188ec
Jack Bridger <jacksbridger@gmail.com>
2019-08-04 14:43:27 +0100

    remove redundant test

--- test/router.test.js
+++ test/router.test.js
@@ -2,12 +2,6 @@ const supertest = require("supertest");
 const tape = require("tape");
 const router = require("../src/router");
 
-tape("Initialise", t => {
-  let num = 2;
-  t.equal(num, 2, "should equal 2");
-  t.end();
-});
-
 tape("Home route returns a status code of 200", t => {
   supertest(router)
     .get("/")
